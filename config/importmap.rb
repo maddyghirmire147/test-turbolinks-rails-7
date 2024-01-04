49a32818cb1e5f96fd73b0feb91fe901cf087881
Maddy Ghirmire <maddy.ghirmire@rbc.com>
2024-01-04 13:11:05 -0600

    install turbolinks and use it as we are using

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,4 +2,5 @@
 
 pin "application"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "turbolinks", to: "https://ga.jspm.io/npm:turbolinks@5.2.0/dist/turbolinks.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
